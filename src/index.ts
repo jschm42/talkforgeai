e338397f181174aa59e98586df3d9181a12f81fe
Jean Schmitz <jean@schmitzonline.name>
2023-04-06 10:33:26 +0200

    Window size

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ if (require('electron-squirrel-startup')) {
 const createWindow = (): void => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    height: 1024,
+    height: 768,
     width: 1280,
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
