69d6b17a3a61278a8d393c50f95f7b164b2280a3
jschm42 <jschm42@users.noreply.github.com>
2023-04-07 19:24:11 +0200

    Process comunication

--- src/store/index.store.ts
+++ src/store/index.store.ts
@@ -5,14 +5,17 @@ import {IndexEntry} from '../service/chat-index.service';
 const indexStore = reactive({
   entries: [] as Array<IndexEntry>,
   load() {
+    // @ts-ignore
     window.chatIndexAPI.listenToLoadReply((entries: Array<IndexEntry>) => {
       this.entries = [...entries];
       console.log('Index loaded', this.entries);
     });
 
+    // @ts-ignore
     window.chatIndexAPI.load();
   },
   save() {
+    // @ts-ignore
     window.chatIndexAPI.save(this.entries);
     console.log('Index saved', this.entries);
   },
