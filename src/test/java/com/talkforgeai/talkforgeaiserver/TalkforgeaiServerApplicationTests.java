0f4891e5526656e58c9232bdbbe4b3b3175b2c3b
jschm42 <jschm42@users.noreply.github.com>
2023-05-23 22:39:19 +0200

    Initial commit

---
+++ src/test/java/com/talkforgeai/talkforgeaiserver/TalkforgeaiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talkforgeai.talkforgeaiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkforgeaiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
