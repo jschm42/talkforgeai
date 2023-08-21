129b4ac5ee31a5e1cb89746c38ff2f2332a34d80
Jean Schmitz <jean@schmitzonline.name>
2023-08-21 16:34:45 +0200

    docker

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
 # We will use OpenJDK 20
-FROM openjdk:20
+FROM ghcr.io/graalvm/jdk-community:20.0.1
 
 # 8080 is the port number the application will use
 EXPOSE 8090
+
+# Set up a volume for the data directory
+VOLUME /data
 
 # copy jar
 COPY ./app/target/app-*.jar /usr/local/lib/talkforgeai.jar
 
 # startup command
-ENTRYPOINT ["java","-jar","/usr/local/lib/talkforgeai.jar"]
+CMD java -Dtalkforge.data-directory=/data -jar /usr/local/lib/talkforgeai.jar --spring.config.additional-location=/usr/local/talkforgeai/talkforgeai.properties
--- backend/src/main/java/com/talkforgeai/backend/storage/FileStorageService.java
+++ backend/src/main/java/com/talkforgeai/backend/storage/FileStorageService.java
@@ -1,14 +1,12 @@
 package com.talkforgeai.backend.storage;
 
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,15 +15,31 @@ import java.nio.file.Paths;
 public class FileStorageService {
     public static final String TALK_FORGE_DIR = ".talkforgeai";
     public static final Logger LOGGER = LoggerFactory.getLogger(FileStorageService.class);
-    private final String fileBasePath = "src/main/resources/static/persona";
+
+    @Value("${talkforge.data-directory}")
+    private String configDataDirectory;
+
+    private Path dataDirectory;
 
     public FileStorageService() {
     }
 
+    @PostConstruct
+    private void postConstruct() {
+        if (configDataDirectory != null && !configDataDirectory.isEmpty()) {
+            this.dataDirectory = Path.of(configDataDirectory);
+
+        } else {
+            this.dataDirectory = Paths.get(System.getProperty("user.home"))
+                    .resolve(TALK_FORGE_DIR)
+                    .normalize();
+        }
+
+        LOGGER.info("Data directory set to {}", this.dataDirectory);
+    }
+
     public Path getDataDirectory() {
-        return Paths.get(System.getProperty("user.home"))
-                .resolve(TALK_FORGE_DIR)
-                .normalize();
+        return this.dataDirectory;
     }
 
     public Path getPersonaImportDirectory() {
@@ -38,39 +52,19 @@ public class FileStorageService {
 
     public void createDataDirectories() {
         try {
-            Files.createDirectories(getDataDirectory());
-            Files.createDirectories(getPersonaDirectory());
-            Files.createDirectories(getPersonaImportDirectory());
+            Path createdPath = Files.createDirectories(getDataDirectory());
+            LOGGER.info("Created data directory {}", createdPath);
+
+            createdPath = Files.createDirectories(getPersonaDirectory());
+            LOGGER.info("Created persona directory {}", createdPath);
+
+            createdPath = Files.createDirectories(getPersonaImportDirectory());
+            LOGGER.info("Created persona import directory {}", createdPath);
 
             LOGGER.info("Directories created successfully");
         } catch (IOException e) {
             LOGGER.error("Failed to create directory: " + e.getMessage());
         }
     }
-
-    public Resource loadAsResource(String filename) {
-        try {
-            var file = Paths.get(fileBasePath).resolve(filename).normalize();
-            Resource resource = new UrlResource(file.toUri());
-            if (resource.exists() || resource.isReadable()) {
-                return resource;
-            } else {
-                throw new RuntimeException("Could not read file: " + filename);
-            }
-        } catch (MalformedURLException e) {
-            throw new RuntimeException("Could not read file: " + filename, e);
-        }
-    }
-
-    public Resource loadAsFileResource(String filename) {
-        var file = Paths.get(fileBasePath).resolve(filename).normalize();
-        Resource resource = new FileSystemResource(file);
-        if (resource.exists() || resource.isReadable()) {
-            return resource;
-        } else {
-            throw new RuntimeException("Could not read file: " + filename);
-        }
-    }
-
 
 }
