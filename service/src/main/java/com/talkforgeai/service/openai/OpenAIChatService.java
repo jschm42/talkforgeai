2884d0624b57167a07bcca04cf58a84ab71c0c65
Jean Schmitz <jean@schmitzonline.name>
2023-10-31 12:49:30 +0100

    Unused methods removed

--- service/src/main/java/com/talkforgeai/service/openai/OpenAIChatService.java
+++ service/src/main/java/com/talkforgeai/service/openai/OpenAIChatService.java
@@ -25,20 +25,16 @@ import com.talkforgeai.service.properties.OpenAIProperties;
 import okhttp3.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.io.IOException;
-import java.util.Optional;
-import java.util.concurrent.Executor;
 
 @Service
 public class OpenAIChatService {
@@ -49,37 +45,15 @@ public class OpenAIChatService {
 
     private final WebClient webClient;
 
-    private final Executor taskExecutor;
-
     @Value("${server.servlet.async.timeout:10000}")
     private long asyncTimeout;
 
     public OpenAIChatService(OpenAIProperties openAIProperties,
-                             OkHttpClient client, @Qualifier("sseTaskExecutor") Executor taskExecutor,
+                             OkHttpClient client,
                              WebClient.Builder webClientBuilder) {
         this.openAIProperties = openAIProperties;
         this.client = client;
-        this.taskExecutor = taskExecutor;
         this.webClient = webClientBuilder.build();
-    }
-
-    ExchangeFilterFunction logRequest() {
-        return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
-            if (LOGGER.isDebugEnabled()) {
-                StringBuilder sb = new StringBuilder("Request: \n");
-                //append clientRequest method and url
-                clientRequest
-                        .headers()
-                        .forEach((name, values) -> values.forEach(value -> {
-                            /* append header key/value */
-                            sb.append(name).append(":").append(value).append("\n");
-                        }));
-                LOGGER.debug(sb.toString());
-
-                LOGGER.debug("RequestBody: ", clientRequest.body());
-            }
-            return Mono.just(clientRequest);
-        });
     }
 
     public OpenAIChatResponse submit(OpenAIChatRequest openAIRequest) {
@@ -189,34 +163,4 @@ public class OpenAIChatService {
                 });
     }
 
-    private String choiceToJSON(OpenAIChatStreamResponse.StreamResponseChoice choice) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            return objectMapper.writeValueAsString(choice);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Cannot parse choice.", e);
-        }
-    }
-
-    private Optional<OpenAIChatStreamResponse.StreamResponseChoice> parseLine(String line) {
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        int jsonStartIndex = line.indexOf('{');
-        if (jsonStartIndex == -1) {
-            return Optional.empty();
-        }
-
-        String jsonContent = line.substring(jsonStartIndex);
-
-        try {
-            OpenAIChatStreamResponse openAIChatStreamResponse = objectMapper.readValue(jsonContent, OpenAIChatStreamResponse.class);
-            OpenAIChatStreamResponse.StreamResponseChoice streamResponseChoice = openAIChatStreamResponse.choices().get(0);
-            if (streamResponseChoice.finishReason() == OpenAIChatStreamResponse.StreamResponseChoice.FinishReason.STOP) {
-                return Optional.empty();
-            }
-            return Optional.of(streamResponseChoice);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Error while parsing chunk line from stream.", e);
-        }
-    }
 }
