4e0c3bb27626ddf0a874b2f5369758a901e61077
Jean Schmitz <jean@schmitzonline.name>
2024-03-05 16:22:43 +0100

    Added vuetify as dependency

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -24,9 +24,18 @@ import 'highlight.js/styles/atom-one-dark.css';
 
 import '@/custom/LaTeXComponent.js';
 import App from '@/components/App.vue';
+import {createVuetify} from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 const app = createApp(App);
 
+app.use(vuetify);
 app.use(router);
 app.use(useStore);
 app.mount('#app');
